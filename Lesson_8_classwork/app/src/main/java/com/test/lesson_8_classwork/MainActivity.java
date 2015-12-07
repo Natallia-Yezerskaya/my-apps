c1f2371d2e2e817b20e0f2431c2d887b838651da
Natallia Yezerskaya <None>
2015-12-07 16:20:09 +0200

    homework

--- Lesson_8_classwork/app/src/main/java/com/test/lesson_8_classwork/MainActivity.java
+++ Lesson_8_classwork/app/src/main/java/com/test/lesson_8_classwork/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.test.lesson_8_classwork.database.DBHelper;
 
@@ -18,20 +19,19 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         final DBHelper db = new DBHelper(this);
+
         db.saveDoctors("Doctor 1",25);
         db.saveDoctors("Doctor 2",30);
         db.saveDoctors("Doctor 3",40);
 
-
-
+        final EditText editText_name = (EditText) findViewById(R.id.editText_name);
+        final EditText editText_age = (EditText) findViewById(R.id.editText_age);
         final Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                    db.getDoctors();
-
-
+                db.updateDoctors(editText_name.getText().toString(),Integer.parseInt(editText_age.getText().toString()));
+                db.getDoctors();
 
             }
         });
--- Lesson_8_classwork/app/src/main/java/com/test/lesson_8_classwork/database/DBHelper.java
+++ Lesson_8_classwork/app/src/main/java/com/test/lesson_8_classwork/database/DBHelper.java
@@ -34,31 +34,55 @@ public class DBHelper extends SQLiteOpenHelper {
         this.mDatabase = getWritableDatabase();
     }
 
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-        Log.d(TAG,"onCreate");
+        Log.d(TAG, "onCreate");
         db.execSQL("CREATE TABLE '"+ DoctorTable.TABLE_NAME +"' ("+
                         DoctorTable.COL_ID  +"  INTEGER PRIMARY KEY AUTOINCREMENT, " +
                         DoctorTable.COL_NAME+" TEXT, " +
-                        DoctorTable.COL_AGE + "INTEGER);");
-
-
-
+                        DoctorTable.COL_AGE + " INTEGER);");
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        Log.d(TAG,"onUpdate");
+        Log.d(TAG, "onUpdate");
     }
-
-
 
     public void saveDoctors(String name, int age){
 
-
-        mDatabase.execSQL("INSERT INTO '"+DoctorTable.TABLE_NAME+"' ('"+DoctorTable.COL_NAME+"', '"+ DoctorTable.COL_AGE+"') VALUES ('" +name+"','"+ age+"')");
+        mDatabase.execSQL("INSERT INTO '" + DoctorTable.TABLE_NAME +
+                "' ('" + DoctorTable.COL_NAME + "', '" + DoctorTable.COL_AGE + "') VALUES ('" + name + "', '" + age + "')");
     }
+
+    public void updateDoctors(String name, int age){
+
+        String query = String.format("select %s, %s from %s where %s = ?",
+                DoctorTable.COL_ID,
+                DoctorTable.COL_NAME,
+                DoctorTable.TABLE_NAME,
+                DoctorTable.COL_NAME);
+        Cursor cursor = mDatabase.rawQuery(query, new String[] {name});
+        if (cursor.moveToNext()) {
+                int id = cursor.getInt(cursor
+                        .getColumnIndex(DoctorTable.COL_ID));
+
+                String queryUpdate = String.format("UPDATE %s SET %s  = ?  where %s = ?",
+                        DoctorTable.TABLE_NAME,
+                        DoctorTable.COL_AGE,
+                        DoctorTable.COL_ID);
+                mDatabase.execSQL(queryUpdate,new Object[] {age, id});
+                Log.i(TAG, "ROW " + id + " HAS age" + age);
+            cursor.close();
+        }
+        else {
+            saveDoctors(name, age);
+        }
+    }
+
+
+
     public void getDoctors(){
         Cursor cursor;
         cursor = mDatabase.query(DoctorTable.TABLE_NAME,
@@ -68,12 +92,11 @@ public class DBHelper extends SQLiteOpenHelper {
         if (cursor.moveToFirst()){
             while (cursor.moveToNext()){
                 Log.d(TAG,"============|" + i +" | ===========");
-                Log.d(TAG,"id"+cursor.getInt(cursor.getColumnIndex(" DoctorTable.COL_ID")));
-                Log.d(TAG,"name"+cursor.getString(1));
-                Log.d(TAG,"age"+cursor.getInt(2));
+                Log.d(TAG,"id "+cursor.getInt(cursor.getColumnIndex(DoctorTable.COL_ID)));
+                Log.d(TAG,"name "+cursor.getString(1));
+                Log.d(TAG,"age "+cursor.getInt(2));
                 i++;
             }
-
         }
         else {
             Log.d(TAG,"error with moveToFirst");
